538a6c8ce39eb019ef9fb7fd1516d3b70397cdef
Matt Cowgill <mattcowgill@gmail.com>
2020-09-03 11:41:06 +1000

    first test

--- tests/testthat/test-viz_corelogic.R
+++ tests/testthat/test-viz_corelogic.R
@@ -1,3 +1,8 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("viz_corelogic_shutdown() creates a graph", {
+  corelogic_url <- "https://raw.githubusercontent.com/MattCowgill/macro_dashboard_data/master/data/corelogic_daily.csv"
+
+  corelogic_graph <- viz_corelogic_shutdown(corelogic_url)
+
+  expect_s3_class(corelogic_graph, "ggplot")
+  expect_s3_class(corelogic_graph, "patchwork")
 })
