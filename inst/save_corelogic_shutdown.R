2e1a44612d55c859ef36c2378e5f171667e08a46
Matt Cowgill <mattcowgill@gmail.com>
2020-09-09 10:30:37 +1000

    save charts

--- inst/save_corelogic_shutdown.R
+++ inst/save_corelogic_shutdown.R
@@ -6,5 +6,7 @@ load_data(named_urls) %>%
   .$corelogic %>%
   filter(date >= as.Date("2020-01-01")) %>%
   viz_corelogic_shutdown() %>%
-  grattan_save(object = ., filename = "inst/charts/test.png",
-               type = "blog", save_pptx = T)
+  grattan_save(object = .,
+               filename = here::here("inst", "charts", "test.png"),
+               type = "blog",
+               save_pptx = T)
