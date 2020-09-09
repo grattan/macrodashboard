1899f78fedd19a7fc7dca6ee09ecded67d8b092d
Matt Cowgill <mattcowgill@gmail.com>
2020-09-09 10:43:09 +1000

    try again

--- inst/save_corelogic_shutdown.R
+++ inst/save_corelogic_shutdown.R
@@ -1,11 +1,9 @@
-library(dplyr)
-library(ggplot2)
-devtools::load_all()
 
-load_data(named_urls) %>%
+
+macrodashboard::load_data(named_urls) %>%
   .$corelogic %>%
   filter(date >= as.Date("2020-01-01")) %>%
-  viz_corelogic_shutdown() %>%
+  macrodashboard::viz_corelogic_shutdown() %>%
   grattan_save(object = .,
                filename = here::here("inst", "charts", "test.png"),
                type = "blog",
